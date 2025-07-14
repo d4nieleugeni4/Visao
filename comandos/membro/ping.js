d345e01a0ed31c26108dd52e8347458bd1136569
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-07-14 19:44:17 -0300

    Update ping.js

--- comandos/membro/ping.js
+++ comandos/membro/ping.js
@@ -1,7 +1,14 @@
+const config = require('../../config/config');
+
 module.exports = {
   name: "ping",
   description: "Responde com pong!",
+  emoji: "🏓",
   execute: async (sock, from) => {
-    await sock.sendMessage(from, { text: "pong!" });
+    await sock.sendMessage(from, { 
+      text: `${config.bot.emoji} *${config.bot.name}* ${config.bot.emoji}\n\n` +
+            `🏓 *Pong!*\n` +
+            `⚡ Versão: ${config.bot.version}`
+    });
   }
 };
